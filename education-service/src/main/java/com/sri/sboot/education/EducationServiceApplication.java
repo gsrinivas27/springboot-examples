f7a591fec9a51980482cebc7ba27a5360a0fcb1e
gsrinivas27 <gsrinivasulu27@gmail.com>
2018-08-25 21:36:16 +0530

    moved config files to separate project

--- education-service/src/main/java/com/sri/sboot/education/EducationServiceApplication.java
+++ education-service/src/main/java/com/sri/sboot/education/EducationServiceApplication.java
@@ -2,10 +2,10 @@ package com.sri.sboot.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class EducationServiceApplication {
 
 	public static void main(String[] args) {
--- person-service/src/main/java/com/sri/sboot/person/PersonServiceApplication.java
+++ person-service/src/main/java/com/sri/sboot/person/PersonServiceApplication.java
@@ -3,9 +3,10 @@ package com.sri.sboot.person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class PersonServiceApplication {
 
 	public static void main(String[] args) {
